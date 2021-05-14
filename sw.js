2153e4cbd8c33a16ef2b20a9c90eccd6905453b2
Jack Lin <jackcylin@gmail.com>
2021-05-14 23:03:06 +0800

    subscribe

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('push', () => {
-    self.registration.sendNotification('test message', {})
-})
+function displayNotification() {
+    if (Notification.permission == 'granted') {
+      navigator.serviceWorker.getRegistration().then(function(reg) {
+        var options = {
+          body: 'Here is a notification body!',
+          icon: 'images/example.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          }
+        };
+        reg.showNotification('Hello world!', options);
+      });
+    }
+  }

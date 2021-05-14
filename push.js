384dba53cb740aa20f877b40c4b88e8aa00f7b31
Jack Lin <jackcylin@gmail.com>
2021-05-14 23:43:03 +0800

    fix

--- push.js
+++ push.js
@@ -12,7 +12,8 @@ push.setVapidDetails('mailto:jackcylin@gmail.com', vapiKeys.publicKey, vapiKeys.
 
 let sub = {
     "endpoint": "https://fcm.googleapis.com/fcm/send/fbvwKLxItMM:APA91bHMYr2yLbsQUwobQ5LBDtKrDwGuigNFm3ubCqzyvi50vmvhjy2Bwcc7pUeSowprlTFRW7z5TkHdgj5xmNIaoqt-X2rra0v81GAQA2tbrTTM9s40oruJPNUK_NK06z9gp4iKNF7F", "expirationTime": null, "keys": {
-        "p256dh": "BO8l7rR7Kc2queugrPsc56ZrFN-iS2N9KsM1oYNgCDIQQglpc_by5dclqWQMP76cFYZ-MU8M1BLQo6w3m3B58D0", "auth": "DV41ZA1DIdUBBqBvF2-F9A"
+        "p256dh": "BO8l7rR7Kc2queugrPsc56ZrFN-iS2N9KsM1oYNgCDIQQglpc_by5dclqWQMP76cFYZ-MU8M1BLQo6w3m3B58D0",
+        "auth": "DV41ZA1DIdUBBqBvF2-F9A"
     }
 }
 push.sendNotification(sub, "test message")
--- sw.js
+++ sw.js
@@ -1,16 +1,24 @@
-function displayNotification() {
-    if (Notification.permission == 'granted') {
-      navigator.serviceWorker.getRegistration().then(function(reg) {
-        var options = {
-          body: 'Here is a notification body!',
-          icon: 'images/example.png',
-          vibrate: [100, 50, 100],
-          data: {
+self.addEventListener('push', function (e) {
+    var options = {
+        body: 'This notification was generated from a push!',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
             dateOfArrival: Date.now(),
-            primaryKey: 1
-          }
-        };
-        reg.showNotification('Hello world!', options);
-      });
-    }
-  }
+            primaryKey: '2'
+        },
+        actions: [
+            {
+                action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close', title: 'Close',
+                icon: 'images/xmark.png'
+            },
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('Hello world!', options)
+    );
+});
